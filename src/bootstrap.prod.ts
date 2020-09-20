92a1a1d6e0b7cb645c21711d86438fa2b062ecb0
craigmellon <craigmellon@etech.net>
2020-09-20 12:20:07 +0000

    update environment for prod

--- src/bootstrap.prod.ts
+++ src/bootstrap.prod.ts
@@ -2,8 +2,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModules } from './app/app.module';
 import { environment } from './environments/environment';
+import { environment as envProd } from './environments/environment.prod';
 
 environment.production = true;
+environment.apiBaseUrl = envProd.apiBaseUrl;
+
 enableProdMode();
 
 platformBrowser()
