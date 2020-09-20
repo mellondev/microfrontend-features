09102a4e129c9e1f6bdd186fa887219dddcc14b1
craigmellon <craigmellon@etech.net>
2020-09-20 10:40:28 +0000

    add azure function for api

---
+++ api/GetFeatures/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+
+const httpTrigger: AzureFunction = async function(context: Context, req: HttpRequest): Promise<void> {
+  context.log('Send features from CosmosDB');
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: context.bindings.features,
+  };
+};
+
+export default httpTrigger;
