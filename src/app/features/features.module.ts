1b5272c9d6903ee1098fb148fe8f9ec5c46a1d29
craigmellon <craigmellon@etech.net>
2020-09-20 11:50:38 +0000

    provide service with module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { FeatureService } from './feature.service';
 import { FeaturesBrowserComponent } from './features-browser/features-browser.component';
 import { FEATURES_ROUTES } from './features.routes';
 import './styles.scss';
@@ -14,6 +15,9 @@ import './styles.scss';
     RouterModule.forChild(FEATURES_ROUTES),
     MatCardModule,
     MatButtonModule
+  ],
+  providers: [
+    FeatureService
   ]
 })
 export class FeaturesModule { }
